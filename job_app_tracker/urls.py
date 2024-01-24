dfee35ca28de9a946a8c89cf477b9fdf03aec309
Zach Allgood <Zach.Allgood@provisionsgroup.com>
2024-01-24 10:29:57 -0600

    Cleaning up the code and starting statistics the page

--- job_app_tracker/urls.py
+++ job_app_tracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('applications/new/', views.create_job_application, name='create_job_application'),
     path('applications/<int:pk>/edit/', views.edit_job_application, name='edit_job_application'),
     path('applications/<int:pk>/delete/', views.delete_job_application, name='delete_job_application'),
+    path('statistics/', views.statistics_view, name='statistics_view'),
     # do not include 'job_app_tracker.urls' or 'settings_and_config.urls' here, as it would cause a circular include
 ]
--- job_app_tracker/views.py
+++ job_app_tracker/views.py
@@ -2,10 +2,33 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import JobApp
 from .forms import JobAppForm
+from django.db.models import Count
 
 def job_application_list(request):
     applications = JobApp.objects.all()
     return render(request, 'job_app_tracker/job_application_list.html', {'applications': applications})
+
+def statistics_view(request):
+    applications_by_status = JobApp.objects.values('status').annotate(status_count=Count('status')).order_by('status')
+    applications_by_company = JobApp.objects.values('company').annotate(company_count=Count('company')).order_by('company')
+
+    status_data = {
+        'labels': [app['status'] for app in applications_by_status],
+        'data': [app['status_count'] for app in applications_by_status],
+    }
+
+    company_data = {
+        'labels': [app['company'] for app in applications_by_company],
+        'data': [app['company_count'] for app in applications_by_company],
+    }
+
+    context = {
+        'applications_by_status': status_data, 
+        'applications_by_company': company_data
+    }
+
+    return render(request, 'job_app_tracker/statistics.html', context)
+
 
 def home(request):
     return render(request, 'home.html')
