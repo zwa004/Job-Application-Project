ab3ad7175eaa83727c61b9c27f24c4ad06f2706d
Zach Allgood <Zach.Allgood@provisionsgroup.com>
2024-02-22 08:19:53 -0600

    Updating version

--- job_app_tracker/forms.py
+++ job_app_tracker/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import JobApp
+from django.forms.widgets import DateInput
 
 class JobAppForm(forms.ModelForm):
+    date_applied = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = JobApp
         fields = ['title', 'company', 'date_applied', 'application_url', 'salary_range', 'experience_level', 'industry','status','employment_type','job_description', 'resume', 'cover_letter']
--- job_app_tracker/models.py
+++ job_app_tracker/models.py
@@ -5,9 +5,10 @@ class JobApp(models.Model):
     title = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
     date_applied = models.DateField()
+    last_modified = models.DateTimeField(auto_now=True)
     application_url = models.CharField(max_length=200)
     experience_level = models.CharField(max_length=200)
-    job_description = models.CharField(max_length=2000)
+    job_description = models.CharField(max_length=2000, blank=True)
     file = models.FileField(upload_to='uploads/', null=True, blank=True)
     resume = models.FileField(upload_to='resumes/', null=True, blank=True)
     cover_letter = models.FileField(upload_to='cover_letters/', null=True, blank=True)
@@ -21,36 +22,8 @@ class JobApp(models.Model):
     ]
 
     employment_type = models.CharField(max_length=20, choices=EMPLOYMENT_TYPE_CHOICES, default='full_time')
-    SALARY_CHOICES = [
-        ('<30000', 'Less than $30,000'),
-        ('30000-35000', '$30,000 - $35,000'),
-        ('35000-40000', '$35,000 - $40,000'),
-        ('40000-45000', '$40,000 - $45,000'),
-        ('45000-50000', '$45,000 - $50,000'),
-        ('50000-55000', '$50,000 - $55,000'),
-        ('55000-60000', '$55,000 - $60,000'),
-        ('60000-65000', '$60,000 - $65,000'),
-        ('65000-70000', '$65,000 - $70,000'),
-        ('70000-75000', '$70,000 - $75,000'),
-        ('75000-80000', '$75,000 - $80,000'),
-        ('80000-85000', '$80,000 - $85,000'),
-        ('85000-90000', '$85,000 - $90,000'),
-        ('90000-95000', '$90,000 - $95,000'),
-        ('95000-100000', '$95,000 - $100,000'),
-        ('100000-105000', '$100,000 - $105,000'),
-        ('105000-110000', '$105,000 - $110,000'),
-        ('110000-115000', '$110,000 - $115,000'),
-        ('115000-120000', '$115,000 - $120,000'),
-        ('120000-125000', '$120,000 - $125,000'),
-        ('125000-130000', '$125,000 - $130,000'),
-        ('130000-135000', '$130,000 - $135,000'),
-        ('135000-140000', '$135,000 - $140,000'),
-        ('140000-145000', '$140,000 - $145,000'),
-        ('145000-150000', '$145,000 - $150,000'),
-        ('>150000', 'More than $150,000'),
-    ]
-    salary_range = models.CharField(max_length=20, choices=SALARY_CHOICES)
-
+    # Change salary range to open text field
+    salary_range = models.CharField(max_length=200)
     EXPERIENCE_CHOICES = [
         ('internship', 'Internship'),
         ('entry', 'Entry Level'),
